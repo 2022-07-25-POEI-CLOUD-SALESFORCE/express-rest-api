896eaf1a48f5c09d719e3facd012bb2499231f91
Christian Lisangola <jochri3@gmail.com>
2022-09-01 17:04:42 +0300

    Add get category by id

---
+++ src/app.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const res = require("express/lib/response");
+
+// Catégories :  /api/categories
+// Produits : /api/products
+
+const categories = [
+  { id: 1, nom: "Jeux vidéos" },
+  { id: 2, nom: "Ordinateurs" },
+];
+
+const app = express();
+
+app.get("/api/categories", (request, response) => {
+  response.send(categories);
+});
+
+app.get("/api/categories/:id", (request, response) => {
+  //   response.send(request.params.id);
+  const id = parseInt(request.params.id);
+
+  //   const category = categories.find(function (category) {
+  //     return category.id === parseInt(id);
+  //   });
+
+  const category = categories.find((category) => category.id === parseInt(id));
+  response.send(category);
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server run on port ${PORT}`);
+});
+
+//CRUD (Create Read Update Delete)
