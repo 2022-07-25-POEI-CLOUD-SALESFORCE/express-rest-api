ac567e3760d3c3204798bca15669cd7c6e1b7a36
Christian Lisangola <jochri3@gmail.com>
2022-09-05 10:36:53 +0300

    Prevent users from creating or updating categories with same name

--- src/app.js
+++ src/app.js
@@ -74,6 +74,16 @@ app.get("/api/categories/:id", validateCategory, (request, response) => {
 
 app.post("/api/categories", [validateBody], (request, response) => {
   const id = generateRandomIndex();
+  const category = categories.find(
+    (category) => category.nom === request.body.nom
+  );
+  if (category) {
+    return response
+      .status(400)
+      .send(
+        `Vous ne pouvez pas avoir 2 catégories avec le nom ${request.body.nom}`
+      );
+  }
   categories.push({ id, ...request.body });
   response.send("Catégorie créée avec succès");
 });
@@ -88,6 +98,16 @@ app.put(
   "/api/categories/:id",
   [validateCategory, validateBody],
   (request, response) => {
+    const category = categories.find(
+      (category) => category.nom === request.body.nom
+    );
+    if (category && category.id !== parseInt(request.params.id)) {
+      return response
+        .status(400)
+        .send(
+          `Vous ne pouvez pas avoir 2 catégories avec le nom ${request.body.nom}`
+        );
+    }
     Object.assign(request.category, request.body);
     response.send("Catégorie mis à jour avec succès");
   }
